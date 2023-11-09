96496186805d39069c2149e05185dde68f2d0818
amitroy4 <amitroyrock6071@gmail.com>
2023-11-10 00:58:10 +0600

    post done

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const app = express()
+const app = express();
 
 const mongoose = require('mongoose');
-
+app.use(express.json());
 mongoose.connect('mongodb+srv://amitroyrock6071:Q4AeKB199aWfeMrR@cluster0.oxq39mj.mongodb.net/ecommerce?retryWrites=true&w=majority')
     .then(() => console.log('DB Connected!'));
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.post("/createtask", function (req, res) {
+    console.log("data",req.body);
+    // res.send('Hello World');
 })
 
 app.listen(8000, function () {
