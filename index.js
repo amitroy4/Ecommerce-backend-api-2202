cdc076fc192d4baa7e22b8cea3a223063f41be45
amitroy4 <amitroyrock6071@gmail.com>
2023-11-09 23:56:28 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("done");
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://amitroyrock6071:Q4AeKB199aWfeMrR@cluster0.oxq39mj.mongodb.net/ecommerce?retryWrites=true&w=majority')
+    .then(() => console.log('DB Connected!'));
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(8000, function () {
+    console.log("server is running");
+})
+
+
+
+// username : amitroyrock6071
+// password: Q4AeKB199aWfeMrR
